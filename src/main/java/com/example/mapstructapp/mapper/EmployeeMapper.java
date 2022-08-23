4c91223003b16ed01e27b485a4b7bd674564c17b
kniewiarowska <62251677+kniewiarowska@users.noreply.github.com>
2022-08-23 16:44:41 +0200

    add inherit configuration

--- src/main/java/com/example/mapstructapp/mapper/EmployeeMapper.java
+++ src/main/java/com/example/mapstructapp/mapper/EmployeeMapper.java
@@ -2,6 +2,7 @@ package com.example.mapstructapp.mapper;
 
 import com.example.mapstructapp.model.Employee;
 import com.example.mapstructapp.model.EmployeeDTO;
+import org.mapstruct.InheritConfiguration;
 import org.mapstruct.Mapper;
 import org.mapstruct.Mapping;
 
@@ -15,4 +16,7 @@ public interface EmployeeMapper {
     @Mapping(target = "tasks", source = "tasks")
     EmployeeDTO toEmployeeDto(Employee employee);
 
+    @InheritConfiguration
+    @Mapping(target = "tasks", ignore = true)
+    EmployeeDTO mapToEmployeeDtoWithoutTasks(Employee employee);
 }
